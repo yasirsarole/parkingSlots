db98c0eefd185acd83573813632629405ca6d15d
yasirprdxn <yasirs@prdxn.com>
2018-04-22 18:14:25 +0530

    added basic structure

---
+++ parkingSlots.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>ParkingSlots | Availability</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="wrapper">
+			<header>
+				<h1>Parking Slots</h1>
+			</header>
+			<main class="parkingSlots">
+				<ul>
+					<li title="slot1"></li>
+					<li title="slot2"></li>
+					<li title="slot3"></li>
+					<li title="slot4"></li>
+					<li title="slot5"></li>
+				</ul>
+				<ul>
+					<li title="slot6"></li>
+					<li title="slot7"></li>
+					<li title="slot8"></li>
+					<li title="slot9"></li>
+					<li title="slot10"></li>
+				</ul>
+			</main>
+		</div>		
+	</body>
+</html>
